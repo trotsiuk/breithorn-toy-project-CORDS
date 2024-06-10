7bd9b9ac2044dd577db234027a17779a331b6ff6
Trotsiuk <volodymyr.trotsiuk@wsl.ch>
2024-06-10 11:04:17 +0200

    initial comming - melt function

---
+++ scripts/melt.R
@@ -0,0 +1,19 @@
+calculate_melt <- function( temperature, melt_factor ) {
+  #' @description calculate the melting rate of the glacier
+  #' @param temperature average temperature
+  #' @param melt_factro a melting factor.
+  #' @example calculate_melt(temperature = 5, melt_factor = 0.1)
+  
+  if (temperature >= 0) {
+    
+    out <- melt_factor * temperature
+    
+  } else {
+    
+    out <- 0
+    
+  }
+  
+  returnt( out )
+}
+
