668c16d059c10871dc410c6bfd9dcc48aa78634d
Fabian Bernhard <fabian.bernhard@unibe.ch>
2024-06-10 11:07:43 +0200

    doc: Add units to melt()

--- scripts/melt.R
+++ scripts/melt.R
@@ -1,7 +1,7 @@
 calculate_melt <- function( temperature, melt_factor ) {
-  #' @description calculate the melting rate of the glacier
+  #' @description calculate the melting rate of the glacier (m/day)
   #' @param temperature average temperature
-  #' @param melt_factro a melting factor.
+  #' @param melt_factro a melting factor (m/day/degree).
   #' @example calculate_melt(temperature = 5, melt_factor = 0.1)
   
   if (temperature >= 0) {
